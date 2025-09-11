328d51c5d8e8f6dcbacba6984666ea493a8b9398
GDemets <gdemets@ensc.fr>
2025-09-11 23:01:31 +0900

    [UPDTAE]-SNAKE GAME

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Example file showing a basic pygame "game loop"
 import pygame
 
 # pygame setup
@@ -9,27 +8,48 @@ running = True
 
 item_pos_x = 225
 item_pos_y = 0
-
-
+alive = True
+direction = None 
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((255,255,255))
 
-    ############ RENDER YOUR GAME HERE ############
-    pygame.draw.circle(screen, (255,0,0), (item_pos_x,item_pos_y), 10)
-    item_pos_y += 1
+    screen.fill((255, 255, 255))
+
+    snake = pygame.draw.circle(screen, (255,0,0), (item_pos_x, item_pos_y), 10)
+
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_q]:
+    if keys[pygame.K_a] and direction != "right":
+        direction = "left"
+    if keys[pygame.K_d] and direction != "left":
+        direction = "right"
+    if keys[pygame.K_w] and direction != "down":
+        direction = "up"
+    if keys[pygame.K_s] and direction != "up":
+        direction = "down"
+
+    if direction == "left":
         item_pos_x -= 1
-    if keys[pygame.K_d]:
+    if direction == "right":
         item_pos_x += 1
-    
-    ###############################################
+    if direction == "up":
+        item_pos_y -= 1
+    if direction == "down":
+        item_pos_y += 1
+
+    if item_pos_x > 450:
+        item_pos_x = 0
+    if item_pos_x < 0:
+        item_pos_x = 450
+    if item_pos_y > 620:
+        item_pos_y = 0
+    if item_pos_y < 0:
+        item_pos_y = 620
+
     pygame.display.flip()
-    clock.tick(60) 
+    clock.tick(60)
 
 pygame.quit()
